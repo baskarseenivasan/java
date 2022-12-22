b5cbff1e206867f7f8f631aa4a12d3b35abb5f1e
baskarseenivasan <112958930+baskarseenivasan@users.noreply.github.com>
2022-12-22 09:25:56 +0530

    second

---
+++ ElectricityBill.java
@@ -0,0 +1,78 @@
+package tamilnadu.chennai;
+
+import java.util.Scanner;
+
+public class ElectricityBill {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Occupation such as Farmer, Handloom, others");
+		String occupation=sc.nextLine();
+		sc.nextLine();
+	
+		
+		System.out.println("Total no of units");
+		int unit = sc.nextInt();
+		 String occupation1="Farmer";
+		 String occupation2="Handloom";
+		 String occupation3="others";
+		 if(occupation1.equals(occupation))
+		 {	
+		//if(unit<=100)
+		//{
+			System.out.println("your unit is free");
+		}
+		if(occupation2.equals(occupation))
+			if(unit<=200)
+		{
+			System.out.println("your unit is free");
+		}
+		//else if(unit<=200)
+		//{
+			//System.out.println("bill of amount is "+((100*0)+(unit-100)*2.25));
+		//}
+		else if(unit<=300)
+		{
+			System.out.println("bill of amount is "+((100*0)+(100*2.25)+(unit-200)*4.50));
+		}
+		else if(unit<=400)
+		{
+			System.out.println("bill of amount is "+((100*0)+(100*2.25)+(100*4.50)+(unit-300)*4.50));
+		}
+		else if(unit<=500)
+		{
+			System.out.println("bill of amount is "+((100*0)+(100*2.25)+(100*4.50)+(100*4.50)+(unit-400)*6));
+		}
+		else if(unit<=600)
+		{
+			System.out.println("bill of amount is "+((100*0)+(100*2.25)+(100*4.50)+(100*4.50)+(100*6)+(unit-500)*8));
+
+		}
+		else if(unit<=700)
+		{
+			System.out.println("bill of amount is "+((100*0)+(100*2.25)+(100*4.50)+(100*4.50)+(100*6)+(100*8)+(unit-600)*9));
+
+		}
+		else if(unit<=800)
+		{
+			System.out.println("bill of amount is "+((100*0)+(100*2.25)+(100*4.50)+(100*4.50)+(100*6)+(100*8)+(100*9)+(unit-700)*9));
+
+		}
+		else if(unit<=900)
+		{
+			System.out.println("bill of amount is "+((100*0)+(100*2.25)+(100*4.50)+(100*4.50)+(100*6)+(100*8)+(100*9)+(100*9)+(unit-800)*9));
+		
+		}
+		else
+		{
+			System.out.println("bill of amount is "+((100*0)+(100*2.25)+(100*4.50)+(100*4.50)+(100*6)+(100*8)+(100*9)+(100*9)+(unit-900)*11));
+		}
+		else
+		{
+			System.out.println("please check your entered occupation");
+		}
+		
+		
+	}
+
+}
+
